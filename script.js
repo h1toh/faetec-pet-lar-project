988c698c1cd592948488e7c85ed98acb469c8fe2
Lucas Gomes <lukashitoh@gmail.com>
2025-09-24 22:06:40 -0300

    mudando fonte, mudando cores, adicionandohover em alguns lugares e resolvendo problemas

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const cidadesDoRioDeJaneiro = ["Angra dos Reis", "Aperibé", "Araruama", "Areal", "Aramação de Búzios", "Arraial do Cabo", "Barra do Piraí", " Barra Mansa", "Belford Roxo", "Bom Jardim", "Bom Jesus do Itapaboana", "Cachoeiras de Macacu", "Cambuci", "Campos dos Goytacazes", "Cantagalo", "Carapebus", "Cardoso Moreira", "Carmo", "Casimiro de Abreu", "Comendador Levy Gasparian", "Cordeiro", "Duas Barras", "Duque de Caxias", "Engenheiro Paulo de Frontin", "Guapimirim", "Iguaba Grande", "Italva", "Itaocara", "Itaperuna", "Itatiaia", "Japeri", "Laje de Muriaé", 'Macaé', "Macuco", "Magé", "Mangaratiba", "Maricá", "Mendes", "Mesquita", " Miguel Pereira", "Miracema", "Natividade", "Nilópolis", "Niterói", "Nova Friburgo", "Nova Iguaçu", "Paracambi", "Paraíba do Sul", "Paraty", "Paty dos Alfares", "Pinheiral", "Piraí", "Porciúncula", "Porto Real", "Quatis", "Queimados", " Quissamã", "Resende", "Rio Bonito", "Rio Claro", "Rio das Flores", "Rio das Ostras", "Rio de Janeiro", "Santa Maria Madalena", "Santo Ântonio de Pádua", "São Fidélis", "São Francisco de Itabapoana", "São Gonçalo", "São João da Barra", "São João de Meriti", "São Joao de Ubá", "São José do Vale do Rio Preto", "São Pedro da Aldeia", "São Sebastião do Alto", "Sapucaia", "Seropédica", "Silva Jardim", "Sumidouro", "Tanguá", "Teresópolis", "Trajano de Moraes", "Três Rios", "Valença", "Varre-Sal", "Vassouras", "Volta Redonda"]
-const especiesParaDoacao = ["Cachorro", "Gato", "Jabuti", "Peixe", "Hamster", "Coelho"]
+
+const especiesParaDoacao = ["Cachorro", "Gato", "Tartaruga", "Peixe", "Hamster", "Coelho"]
+
+const temperamentosDoAnimal = ["Tranquilo", "Sociavel", "Medroso", "Brincalhão", "Ativo", "Agressivo", "Feroz", "Dócil"]
+
 const selectCidades = document.getElementById('cidades')
 const selectEspecie = document.getElementById('especies')
+const selectTemperamento = document.getElementById('temperamento')
 const uploadDiv = document.getElementById('picture-background')
 const fileInput = document.getElementById('fileInput')
 const preview = document.getElementById('preview')
 const camera = document.getElementById('camera')
 const textoImg = document.getElementById('textoImg')
+const outroTemperamento = document.getElementById('outroTemperamento')
+
+
+const btnClicavel = document.getElementById('btnClicavel').addEventListener('click',()=>{
+            alert('Disponível em breve!')
+})
 
 uploadDiv.addEventListener("click", () => {
     fileInput.click()
 })
 
-fileInput.addEventListener("change", ()=>{
+fileInput.addEventListener("change", () => {
     const file = fileInput.files[0]
-    if(file){
+    if (file) {
         const reader = new FileReader()
-        reader.onload = (e)=>{
-            const img= document.createElement('img')
+        reader.onload = (e) => {
+            const img = document.createElement('img')
             img.src = e.target.result
             preview.innerHTML = ""
             camera.style.display = 'none'
@@ -43,3 +54,9 @@ especiesParaDoacao.forEach(especie => {
     selectEspecie.appendChild(optionEspecies)
 })
 
+temperamentosDoAnimal.forEach(temperamento => {
+    const optionTemperamento = document.createElement('option')
+    optionTemperamento.value = temperamento
+    optionTemperamento.textContent = temperamento
+    selectTemperamento.appendChild(optionTemperamento)
+})
